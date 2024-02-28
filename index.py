f0b7cba3c75486ec310525c3d56930fc5e08ea9c
Dederiffa <140695004+Dederiffa@users.noreply.github.com>
2024-02-28 14:33:13 +0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,7 @@
+panjang = 5
+lebar = 10,5
+
+print(panjang, '*', lebar, '"', luas)
+print("tipe dari variabel panjang:", type(panjang))
+print("tipe dari variabel lebar:", type(lebar))
+print("tipe dari variabel luas:", type(luas))
